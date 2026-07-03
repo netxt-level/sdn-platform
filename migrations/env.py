7ffb220c84adc737d32773aeeece8c75489a066e
cprtmchzh <bin54511563@gmail.com>
2026-07-03 12:25:24 +0900

    fix: Alembic 스키마 비교 설정 추가

--- migrations/env.py
+++ migrations/env.py
@@ -86,6 +86,7 @@ def run_migrations_offline() -> None:
     context.configure(
         url=url,
         target_metadata=target_metadata,
+        include_schemas=True,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
@@ -109,7 +110,9 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            include_schemas=True,
         )
 
         with context.begin_transaction():
